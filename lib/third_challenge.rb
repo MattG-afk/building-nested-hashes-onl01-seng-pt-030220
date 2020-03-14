ddf0f19a4a00a4712d2f63a344677531486b9015
Matt Gordon <bigmattsize21@gmail.com>
2020-03-14 20:18:29 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,13 +2,12 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague",
+        name: => "Lord Montague",
         age: "53"
         },
       :matriarch => {
-        :name => "Lady Montague",
-        age: => "5y"
-        #your key/value pairs here
+        name: => "Lady Montague",
+        age: => "54"
         },
       :hero => {
         :name => "Romeo"
