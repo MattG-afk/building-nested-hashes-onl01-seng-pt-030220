5ea9bfd3d0ef7f2ff55ee1af03031882bd548e79
Matt Gordon <bigmattsize21@gmail.com>
2020-03-14 20:15:36 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -21,17 +21,19 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        :name => "Lord Montague"
-        :age => "53"
+        :name => "Lord Capulet"
+        :age => "50"
         #your key/value pairs here
         },
       :matriarch => {
-        :name => "Lord Montague"
-        :age => "53"
+        :name => "Lady Capulet"
+        :age => "51"
         #your key/value pairs here
         },
       :heroine => {
-        
+        :name => "Juliet"
+        :age => "15"
+        :status => "alive"
         #your key/value pairs here
         },
       :heroine_friends => []
